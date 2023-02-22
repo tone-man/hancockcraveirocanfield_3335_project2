ff5dedc482e092edb08a91a2a7c853f0c0229ae3
William Hancock <williamhancock328@gmail.com>
2023-02-21 20:00:33 -0500

    created driver code and the view controller

---
+++ src/Main.py
@@ -0,0 +1,3 @@
+import ViewController as vc
+
+vc.initGUI()
---
+++ src/ViewController.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+def initGUI():
+    # create a window that displays 8 images in a 2x4 grid
+    window = tk.Tk()
+    window.title("Genetic Algorithm Visualization")
+    window.geometry("800x600")
+    window.resizable(False, False)
+
+    tk.mainloop()
+
+    
