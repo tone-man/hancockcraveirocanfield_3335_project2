26005a305fd9adac240e9106e7f5bc069470ba60
Toni <craveiroa@merrimack.edu>
2023-02-24 10:50:15 -0500

    Constructor works

--- src/GeneticImages.py
+++ src/GeneticImages.py
@@ -1,66 +1,61 @@
 from FitnessFunctionInterface import FitnessFunctionInterface
 from PIL import Image
+import random
 
 class GeneticImages:
     '''
     This class uses Genetic Algorithms to generate a population of images
     that will evolve to a given fitness function.
     '''
-    self.population
-    self.mixNumber
-    self.mutationRate
-    # self.combinationMode for later
-    self.evolutionStep
-    self.fitnessFunction
 
-    def __init__(f: FitnessFunctionInterface, p: int):
+    def __init__(self, f: FitnessFunctionInterface, p: int):
         self.population = []
         self.mixNumber = 2
         self.mutationRate = 0.01
         self.evolutionStep = 0
-        self.fitnessFunction = f.calcFittness
+        self.fitnessFunction = f.calcFitness
 
         for i in range(p):
             m = Image.new(mode="RGB", size= ( 200, 200 ), color= (255 , 255, 255))
 
             for x in range(200):
                 for y in range(200):
-                    m.putpixel(xy = (x,y), value = __randPix())
+                    m.putpixel(xy = (x,y), value = self.__randPix())
             
             self.population.append(m)
 
 
-    def getPopulation():
+    def getPopulation(self):
         return self.population
 
-    def getFittest():
+    def getFittest(self):
         return self.population[len(self.population) - 1]
 
-    def getStep():
+    def getStep(self):
         return self.evolutionStep
     
-    def setPopulationSize():
+    def setPopulationSize(self):
         pass
 
-    def setFitnessFunction():
+    def setFitnessFunction(self):
         pass
 
-    def __calcFittness(m: Image) -> float:
+    def __calcFittness(self, m: Image) -> float:
         return self.fitnessFunction(m)
     
-    def __selectParent():
+    def __selectParent(self):
         pass
 
-    def __crossMembers():
+    def __crossMembers(self):
         pass
 
-    def __mutate():
+    def __mutate(self):
         pass
 
-    def __step():
+    def __step(self):
         pass
 
-    def __randPix(p: int):
+    def __randPix(self):
         return (random.randint(0,255) , random.randint(0,255), random.randint(0,255) )
 
 
--- src/Main.py
+++ src/Main.py
@@ -1,3 +1,11 @@
 import ViewController as vc
+import GeneticImages
+import FitnessFunctions as ff
+import PIL
 
-vc.initGUI()
+f = ff.CircleFitnessFunction()
+g = GeneticImages.GeneticImages(f, 8)
+
+p = g.getPopulation()
+
+p[0].show()
