83e15747f739fbfbca30cb1048a324b4ae11e342
William Hancock <williamhancock328@gmail.com>
2023-02-23 20:00:33 -0500

    Added PIL test

---
+++ src/imgtest.py
@@ -0,0 +1,26 @@
+from PIL import Image  
+import random
+#testing PIL here
+width = 200
+height = 200
+center = (width/2, height/2)
+numpixels = 0
+random.seed(random.randint(0,100000))
+img  = Image.new( mode = "RGB", size = (width, height) )
+#set each pixel to a random color
+for x in range(width):
+    for y in range(height):
+        #generate a random color
+        r = random.randint(0,255)
+        g = random.randint(0,255)
+        b = random.randint(0,255)
+        img.putpixel( (x,y), (r, g, b) )
+#check if pixels are white at a radius of 10 from the center
+for x in range(width):
+    for y in range(height):
+        if (x-center[0])**2 + (y-center[1])**2 < 2**2:
+            if img.getpixel( (x,y) ) != (255,255,255):
+                numpixels += 1
+
+print ("Number of pixels not white at radius 10 from center: ", numpixels)
+img.show()
