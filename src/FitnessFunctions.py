a34adef5524f204fb61b29cc9bd74d5aaa5c900c
William Hancock <williamhancock328@gmail.com>
2023-02-26 21:02:38 -0500

    fitness function created

--- src/FitnessFunctions.py
+++ src/FitnessFunctions.py
@@ -1,7 +1,25 @@
 from FitnessFunctionInterface import FitnessFunctionInterface
 from PIL import Image
+import math
 
 class CircleFitnessFunction(FitnessFunctionInterface):
 
     def calcFitness(m : Image) -> float:
-        pass
+        dfc = []
+        fits = []
+        sz = m.size
+        center = (sz[0]/2, sz[1]/2)
+        for x in range(sz[0]):
+            for y in range(sz[1]):
+                if m.getpixel((x,y)) != (255,255,255):
+                    d = math.sqrt((x-center[0])**2 + (y-center[1])**2)
+                    dfc.append(d)
+        dfc.sort()
+        for i in ([5*x for x in range(10,21)]):
+            dlen = len(dfc)
+            num = dfc.count(i)
+            fitnesshere = num - (dlen - num)
+            fits.append(fitnesshere)
+        
+        fitness = max(fits)
+        print(fitness)
--- src/imgtest.py
+++ src/imgtest.py
@@ -1,10 +1,11 @@
 from PIL import Image  
 import random
+import math
 #testing PIL here
 width = 200
 height = 200
 center = (width/2, height/2)
-numpixels = 0
+dfc = []
 random.seed(random.randint(0,100000))
 img  = Image.new( mode = "RGB", size = (width, height) )
 #set each pixel to a random color
@@ -15,12 +16,20 @@ for x in range(width):
         g = random.randint(0,255)
         b = random.randint(0,255)
         img.putpixel( (x,y), (r, g, b) )
-#check if pixels are white at a radius of 10 from the center
+#check if pixels are white
 for x in range(width):
     for y in range(height):
-        if (x-center[0])**2 + (y-center[1])**2 < 10**2:
-            if img.getpixel( (x,y) ) != (255,255,255):
-                numpixels += 1
-
-print ("Number of pixels not white at radius 10 from center: ", numpixels)
-img.show()
+        if img.getpixel((x,y)) != (255,255,255):
+            d = math.sqrt((x-center[0])**2 + (y-center[1])**2)
+            dfc.append(d)
+dfc.sort()
+fits = []
+n = 5
+for i in ([n*x for x in range(10,21)]):
+  dlen = len(dfc)
+  num = dfc.count(i)
+  fitnesshere = num - (dlen - num)
+  fits.append(fitnesshere)
+  
+fitness = max(fits)
+print(fitness)
