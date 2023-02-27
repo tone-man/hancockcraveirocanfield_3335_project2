c9f290f9f692574246d13fc3a65ad69469157c17
Toni <craveiroa@merrimack.edu>
2023-02-27 10:34:11 -0500

    Elitism reveals a bug
    I have been struggling with a demonic bug in our code that is not appending the proper elites into the next population. Need someone else's eyes on our code to see if they can figure it out.

--- src/FitnessFunctions.py
+++ src/FitnessFunctions.py
@@ -5,23 +5,37 @@ import math
 class CircleFitnessFunction(FitnessFunctionInterface):
 
     def calc_fitness(self, m : Image) -> float:
-        dfc = []
-        fits = []
+        pix = [] #Array of non-white pixel distances
         sz = m.size
         center = (sz[0]/2, sz[1]/2)
+        band_size = 10 #amount of pixels in a given circle
+        fitness = None
+        
+        #Calculate distance of all non-white pixels
         for x in range(sz[0]):
             for y in range(sz[1]):
                 if m.getpixel((x,y)) != (255,255,255):
                     d = math.sqrt((x-center[0])**2 + (y-center[1])**2)
-                    dfc.append(d)
-        dfc.sort()
-        for i in ([5*x for x in range(10,21)]):
-            dlen = len(dfc)
-            num = dfc.count(i)
-            fitnesshere = num - (dlen - num)
-            fits.append(fitnesshere)
+                    pix.append(d)   
+                            
+        #Sort to search
+        pix.sort()
         
-        fitness = max(fits)
+        #Calculate Fitness at given Bands
+        for i in range(10, 100, band_size):
+            all_pix = len(pix)
+            num = 0 #number of pixels in a given band
+            
+            for dist in pix:
+                if dist >= i and dist < (i + band_size):
+                    num += 0
+                    
+            cur_fit = num - (all_pix - num)
+            
+            if not fitness:
+                fitness = cur_fit
+
+            fitness = max(fitness, cur_fit)
         
         return fitness
     
--- src/GeneticImages.py
+++ src/GeneticImages.py
@@ -17,6 +17,10 @@ class GeneticImages:
         self.evolution_step = 0
         self.fitness_func = f
         self.IMAGE_SIZE = 200
+        
+        #Elitism Stuff
+        self.elitism_enabled = True
+        self.elite_count = int(p / 2) # defaults to half
 
         for i in range(self.pop_size):
             m = Image.new(mode="RGB", size= (self.IMAGE_SIZE, self.IMAGE_SIZE ), color= (255 , 255, 255))
@@ -39,6 +43,11 @@ class GeneticImages:
     def step(self, n: int = 1):
         for x in range(n):
             self.__step()
+            
+    def is_started(self):
+        if(self.evolution_step > 0):
+            return True
+        return False
     
     #def setPopulationSize(self):
         #pass
@@ -117,20 +126,32 @@ class GeneticImages:
     def __step(self):
         population2 = [] #new array to hold offspring 
 
-        for m in range(self.pop_size): #calculate fitness for members
+        #calculate fitness for members
+        for m in range(self.pop_size):
             self.fitness[m] = (self.__calc_fittness(self.population[m]))
 
+        #check fitness is valid for weighting
         if(self.fitness_func.is_neg()):
             self.__fix_fittness()
-        
         print("Fitness :", self.fitness)
         
+        #weight fitness for roulette selection
         weights = self.__calc_weights()
-
         print("Weights :", weights)
-        for m in self.population:
+        
+        #check to see if elitism is enabled
+        if self.elitism_enabled:
+            sort_fit = sorted(self.fitness)
+            
+            for x in range(self.pop_size - 1 , (self.pop_size - 1) - self.elite_count, -1):
+                #Get Fitness value
+                f_val = sort_fit[x]
+                #Find value in the fitness array, this isnt perfect and needs work
+                print(f_val, self.fitness.index(f_val))
+                population2.append(self.population[self.fitness.index(f_val)])
+            
+        while len(population2) < self.pop_size:
             parents = self.__select_parents(weights)
-            print("Parents : ", len(parents))
             c = self.__cross_members(parents)
             self.__mutate(c)
 
